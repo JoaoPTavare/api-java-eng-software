c3437d5e9fc0dbe3886c0975734076544ab67a68
JoaoPTavare <jjoaopedrotava05@gmail.com>
2022-09-15 21:38:16 -0300

    Criação de projeto

---
+++ InitializrSpringbootProject/src/main/java/app_java_eng/demo/model/EnderecoUsuario.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package app_java_eng.demo.model;
+
+/**
+ *
+ * @author jptav
+ */
+public class EnderecoUsuario {
+    
+}
