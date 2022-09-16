c403355ac071ec0f9b7fa19c7d826c43b192b0f1
JoaoPTavare <jjoaopedrotava05@gmail.com>
2022-09-15 21:53:53 -0300

    Criação da tabela Endereço do Usuário e correção da falata de fechamento

--- InitializrSpringbootProject/src/main/java/app_java_eng/demo/model/EnderecoUsuario.java
+++ InitializrSpringbootProject/src/main/java/app_java_eng/demo/model/EnderecoUsuario.java
@@ -9,5 +9,53 @@ package app_java_eng.demo.model;
  * @author jptav
  */
 public class EnderecoUsuario {
+ 
+    
+    private Integer codEndereco;
+    private String endereco;
+    private int numEndereco;
+
+    public EnderecoUsuario() {
+    }
+
+    public EnderecoUsuario(Integer codEndereco, String endereco, int numEndereco) {
+        this.codEndereco = codEndereco;
+        this.endereco = endereco;
+        this.numEndereco = numEndereco;
+    }
+
+    public Integer getCodEndereco() {
+        return codEndereco;
+    }
+
+    public void setCodEndereco(Integer codEndereco) {
+        this.codEndereco = codEndereco;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getNumEndereco() {
+        return numEndereco;
+    }
+
+    public void setNumEndereco(int numEndereco) {
+        this.numEndereco = numEndereco;
+    }
+
+    @Override
+    public String toString() {
+        return "EnderecoUsuario{" + "codEndereco=" + codEndereco + ", endereco=" + endereco + ", numEndereco=" + numEndereco + '}';
+    }
+    
+    
+    
+    
+    
     
 }
