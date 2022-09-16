b55fcdae99a1c52fe86ff80ce803d0a3657399a2
MatheusSantosAguiar <matheusaguiar080@gmail.com>
2022-09-15 22:01:36 -0300

    criaçao de agendamento

--- InitializrSpringbootProject/src/main/java/app_java_eng/demo/model/Agendamento.java
+++ InitializrSpringbootProject/src/main/java/app_java_eng/demo/model/Agendamento.java
@@ -1,12 +1,61 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package app_java_eng.demo.model;
  
 
 
 public class Agendamento {
+    private String nome;
+    private String CNPJ;
+    private String dataNasc;
+    private String telCont;
+
+    public Agendamento() {
+    }
+
+    public Agendamento(String nome, String CNPJ, String dataNasc, String telCont) {
+        this.nome = nome;
+        this.CNPJ = CNPJ;
+        this.dataNasc = dataNasc;
+        this.telCont = telCont;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCNPJ() {
+        return CNPJ;
+    }
+
+    public void setCNPJ(String CNPJ) {
+        this.CNPJ = CNPJ;
+    }
+
+    public String getDataNasc() {
+        return dataNasc;
+    }
+
+    public void setDataNasc(String dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    public String getTelCont() {
+        return telCont;
+    }
+
+    public void setTelCont(String telCont) {
+        this.telCont = telCont;
+    }
+
+    @Override
+    public String toString() {
+        return "Agendamento{" + "nome=" + nome + ", CNPJ=" + CNPJ + ", dataNasc=" + dataNasc + ", telCont=" + telCont + '}';
+    }
+    
+    
     
  
 }
