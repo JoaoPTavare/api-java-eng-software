28955b40e777450e16194c012e244f66e6a4c893
Grimmjowf <ajonathan967@gmail.com.br>
2022-09-17 18:49:17 -0300

    Mapeamento da classe Colaborador

--- InitializrSpringbootProject/src/main/java/app_java_eng/demo/model/Colaborador.java
+++ InitializrSpringbootProject/src/main/java/app_java_eng/demo/model/Colaborador.java
@@ -1,22 +1,51 @@
 
 package app_java_eng.demo.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.hibernate.annotations.GeneratorType;
 
+@Entity
+@Table(name = "colaborador")
 public class colaborador {
     
+    @Id
+    @Column(name = "codColaborador")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long codColaborador;
+    @Column(name = "nome")
     private String nome;
+    
+    @Column(name = "cnpj")
     private String cnpj;
+   
+    @Column(name = "dataNasc")
     private String dataNasc;
+    
+    @Column(name = "telCont")
     private String telCont;
 
     public colaborador() {
     }
 
-    public colaborador(String nome, String cnpj, String dataNasc, String telCont) {
+    public colaborador(long codColaborador, String nome, String cnpj, String dataNasc, String telCont) {
+        this.codColaborador = codColaborador;
         this.nome = nome;
         this.cnpj = cnpj;
         this.dataNasc = dataNasc;
         this.telCont = telCont;
+    }
+
+    public long getCodColaborador() {
+        return codColaborador;
+    }
+
+    public void setCodColaborador(long codColaborador) {
+        this.codColaborador = codColaborador;
     }
 
     public String getNome() {
@@ -53,8 +82,7 @@ public class colaborador {
 
     @Override
     public String toString() {
-        return "colaborador{" + "nome=" + nome + ", cnpj=" + cnpj + ", dataNasc=" + dataNasc + ", telCont=" + telCont + '}';
+        return "colaborador{" + "codColaborador=" + codColaborador + ", nome=" + nome + ", cnpj=" + cnpj + ", dataNasc=" + dataNasc + ", telCont=" + telCont + '}';
     }
-    
-    
+
 }
